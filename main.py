432638fcbf25cb74ab3d86faa3ea8a41b6b03d5c
Yaros <yaros@Yaross-Laptop.local>
2024-01-28 10:26:38 +0100

    Init

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+
+import websockets
+from aiohttp import web
+
+WEB_SOCKETS_URL = os.environ.get('WEB_SOCKETS_URL', 'ws://localhost:3001/healthz')
+
+
+async def healthz_handler(request):
+    async with websockets.connect(WEB_SOCKETS_URL) as websocket:
+        response = await websocket.recv()
+        if response != 'ok':
+            return web.Response(status=500, text="Not OK\n")
+
+    return web.Response(status=200, text="OK\n")
+
+
+async def init_app():
+    app = web.Application()
+    app.router.add_get('/healthz', healthz_handler)
+    return app
+
+
+if __name__ == '__main__':
+    app = init_app()
+    web.run_app(app, host='0.0.0.0', port=8080)
